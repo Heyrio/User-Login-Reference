f526cb7c671cb6b794da0665ed7b6ee1ae846f83
Hamza Amer <hamer1563@gmail.com>
2019-04-30 14:44:50 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+
+
+//DB config
+
+const URI = 'mongodb+srv://heyrio:password@cluster0-7cpbz.mongodb.net/test?retryWrites=true'
+
+mongoose.connect(URI,{useNewUrlParser: true})
+.then(()=>{
+    console.log('MongoDb connected...');
+})
+.catch((err)=>{
+    console.log(err);
+})
+
 //EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+//BodyParser
+app.use(express.urlencoded({ extended: false }))
+
 
 //Routes 
 app.use('/', require("./routes/index"));
