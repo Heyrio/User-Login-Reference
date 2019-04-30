03c005b2d7aa55efa10f3895817d5e1bb1c0fff2
Hamza Amer <hamer1563@gmail.com>
2019-04-30 13:48:06 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Routes 
+app.use('/', require("./routes/index"));
+app.use('/users', require("./routes/users"));
+
+app.listen(port,()=>{
+    console.log('Server is running on port ' + port);
+})
